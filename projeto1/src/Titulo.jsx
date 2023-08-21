7d287bc4f0fd14bd66a567b79d158ecdb591bbe5
perealvees <perealvees@gmail.com>
2023-08-21 15:50:52 -0300

    introdução básica

---
+++ projeto1/src/Titulo.jsx
@@ -0,0 +1,15 @@
+function Titulo () {
+
+    let nome = 'Priscilla Alves';
+
+    return (
+        <div>
+            <h1>Oi, eu sou a {nome}</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime assumenda eius incidunt animi aspernatur. Ratione sapiente error magnam perferendis dolorum incidunt vel asperiores officia nam voluptatum aperiam, ducimus distinctio quam.</p>
+        </div>
+    )
+    
+    
+}
+
+export default Titulo
